0b2f248c526ea5095b8835fa5902bda8dbd2c5bc
Vladimir Tarasenko <umatomba@gmail.com>
2017-01-10 13:22:16 +0000

    initial

---
+++ lib/checkel.ex
@@ -0,0 +1,18 @@
+defmodule Checkel do
+  @moduledoc """
+  Documentation for Checkel.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Checkel.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/checkel_test.exs
@@ -0,0 +1,8 @@
+defmodule CheckelTest do
+  use ExUnit.Case
+  doctest Checkel
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
